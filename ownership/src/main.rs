52dfff32423992147ca3aaf3922d35c0a8c0b102
Kota Minegishi <kotamine@gmail.com>
2025-02-26 05:00:10 +0000

    added third_word fn

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -22,6 +22,26 @@ fn last_word(s: &str) -> &str {
     &s[..]
 }
 
+fn third_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    
+    let mut count = 0;
+    let mut i1 = 0;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            count += 1;
+            if count == 2 {
+                i1 = i;
+            } else if count == 3 {
+                return &s[i1+1..i];
+            }
+        }
+    }
+
+    // return empty string if there are less than 3 words
+    ""
+}
+
 
 fn main() {
     let s = String::from("hello world, great to see you!");
@@ -31,6 +51,9 @@ fn main() {
     let word2 = first_word(&s[6..]);
     println!("after 6+ chr,  the first word is: {word2}"); 
 
-    let word3 = last_word(&s[..]);
-    println!("the last word is: {word3}");
+    let word3 = third_word(&s[..]);
+    println!("the third word is: {word3}");
+
+    let word4 = last_word(&s[..]);
+    println!("the last word is: {word4}");
 }
